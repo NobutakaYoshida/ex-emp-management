64ea1845afd1d5754c50559f54f53a9c49154228
yoshidanobutaka <nobutaka.yoshida@rakus-partners.co.jp>
2021-11-04 09:24:40 +0900

    LoginFormを作成

---
+++ src/main/java/jp/co/sample/form/loginForm.java
@@ -0,0 +1,31 @@
+package jp.co.sample.form;
+
+public class loginForm {
+	
+	// メールアドレス
+	public String mailAddress;
+	// パスワード
+	public String password;
+
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "loginForm [mailAddress=" + mailAddress + ", password=" + password + "]";
+	}
+
+}
